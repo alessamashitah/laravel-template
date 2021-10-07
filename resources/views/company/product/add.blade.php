105453756d08e401f114811d1d393e308a2529aa
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-07 18:00:02 +0800

    create add product page

---
+++ resources/views/company/product/add.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Add Product') }}</div>
+
+
+                <div class="card-body">
+                <form method="POST" action="{{ route('productStore')}}" enctype="multipart/form-data"  >
+                @csrf
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name</label>
+                    <input type="name" name="name" class="form-control" id="name" placeholder="">
+                </div>
+                                    
+                <div class="mb-3">
+                    <label for="formFile" class="form-label">Add Image</label>
+                    <input class="form-control" type="file" name="file" >
+                </div>
+
+                <div class="mb-3">
+                    <label for="qty" class="form-label">Quantity</label>
+                    <input type="qty" name="qty" class="form-control" id="qty" placeholder="">
+                </div>
+
+                <button type="submit" class="btn btn-light">Submit</button>
+                </form>   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
