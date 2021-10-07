f76d062f1128859a097dac880a94c1554953be03
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-07 17:59:46 +0800

    create admin home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <div class="card-header">Dashboard</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    You are Admin.
                 </div>
+
+                
+                <div class="card-body">
+                <a href="{{ route('productIndex')}}" type="button" class="btn btn-dark">Product</a>
+                </div>
+                
             </div>
         </div>
     </div>
 </div>
 @endsection
+
+
