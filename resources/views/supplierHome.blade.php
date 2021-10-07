080df495849aa1dd52f96fc833d7fbb43a949b91
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-07 17:59:31 +0800

    create supplier home

---
+++ resources/views/supplierHome.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+                <div class="card-body">
+                    You are Supplier.
+                </div>
+
+                
+                <div class="card-body">
+                <a href="" type="button" class="btn btn-dark">Component</a>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
