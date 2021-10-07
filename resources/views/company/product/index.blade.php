f87b06111a5b0b54abc6baecd119fee46eb87204
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-07 18:00:27 +0800

    create list product index

---
+++ resources/views/company/product/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @if(session()->has('alert-message'))
+                <div class="alert {{session()->get('alert-type')}}">
+                    {{ session()->get('alert-message') }}
+                </div>
+            @endif
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                <a href="{{ route('productAdd')}}" type="button" class="btn btn-dark">Add Product</a>
+                </div>
+               
+
+                <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Qty</th>
+                        <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($product as $key=>$product)
+                        <tr>
+                        <th scope="row">{{ $key + 1}}</th>
+                        <td>{{$product->name}}</td>
+                        <td>
+                        <img src="{{ asset('/storage/product/'.$product->image) }}" width="50px;" height="50px;" alt="">
+                        </td>
+                        <td>{{$product->qty}}</td>
+                        <td><a href="" type="button" class="btn btn-dark">Edit</a></td>
+                        <td><a href="" type="button" class="btn btn-danger">Delete</a></td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
