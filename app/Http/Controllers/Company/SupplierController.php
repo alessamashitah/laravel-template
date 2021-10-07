78a8e5a281e9c68f7fce3cc9f21476429ccef265
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-07 22:58:35 +0800

    supplier controller updated

--- app/Http/Controllers/Company/SupplierController.php
+++ app/Http/Controllers/Company/SupplierController.php
@@ -7,5 +7,10 @@ use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    //
+    public function index()
+    {
+        
+    
+        return view('company.product.supplier');
+    }
 }
