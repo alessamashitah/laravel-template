9ca9c5f4c0d830a1977f59aa4f1cc2649c879137
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-08 22:25:28 +0800

    remove value for column

--- resources/views/company/product/edit.blade.php
+++ resources/views/company/product/edit.blade.php
@@ -14,17 +14,17 @@
                 @csrf
                 <div class="mb-3">
                     <label for="name" class="form-label">Name</label>
-                    <input type="name" name="name"  class="form-control" id="name" value="" placeholder="name">
+                    <input type="name" name="name"  class="form-control" id="name" value="{{ $product->name }}" placeholder="name">
                 </div>
                                     
                 <div class="mb-3">
                     <label for="formFile" class="form-label">Add Image</label>
-                    <input class="form-control" type="file" name="file" value="" >
+                    <input class="form-control" type="file" name="file" value="{{ $product->image }}" >
                 </div>
 
                 <div class="mb-3">
                     <label for="qty" class="form-label">Quantity</label>
-                    <input type="qty" name="qty" class="form-control" id="qty" value="" placeholder="qty">
+                    <input type="qty" name="qty" class="form-control" id="qty" value="{{ $product->qty }}" placeholder="qty">
                 </div>
 
                 <button type="submit" class="btn btn-light">Submit</button>
