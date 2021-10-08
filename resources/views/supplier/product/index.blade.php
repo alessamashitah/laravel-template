db881b0af48b70996305d1b4b245cd27e7108ee4
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-08 22:27:36 +0800

    create index page for product

---
+++ resources/views/supplier/product/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @if(session()->has('alert-message'))
+                <div class="alert {{session()->get('alert-type')}}">
+                    {{ session()->get('alert-message') }}
+                </div>
+            @endif
+            <div class="card">
+                <div class="card-header">{{ __('List of Products') }}</div>
+
+               
+
+                <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Qty</th>
+                        </tr>
+                    </thead>
+                  
+                </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
