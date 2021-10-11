de1228c3eff8186dc9162da2958029402e97ef8f
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-11 14:17:27 +0800

    add route and column for table

--- resources/views/supplier/product/index.blade.php
+++ resources/views/supplier/product/index.blade.php
@@ -13,7 +13,9 @@
             <div class="card">
                 <div class="card-header">{{ __('List of Products') }}</div>
 
-               
+                <div class="card-body">
+                <a href="{{ route('supplier.home')}}" type="button" class="btn btn-dark">Back</a>
+                </div>
 
                 <div class="card-body">
                 <table class="table">
@@ -23,10 +25,28 @@
                         <th scope="col">Name</th>
                         <th scope="col">Image</th>
                         <th scope="col">Qty</th>
+                        <th scope="col">Components</th>
+                        <th scope="col">Action</th>
                         </tr>
                     </thead>
-                  
-                </table>
+                    <tbody>
+                        @foreach($products as $key=>$product)
+                        <tr>
+                        <th scope="row">{{ $key + 1}}</th>
+                        <td>{{$product->name}}</td>
+                        <td>
+                        <img src="{{ asset('/storage/product/'.$product->image) }}" width="50px;" height="50px;" alt="">
+                        </td>
+                        <td>{{$product->qty}}</td>
+                        <td>
+                            @foreach($product->components as $component)
+                            {{$component->name}},
+                            @endforeach
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    </table>
                 </div>
             </div>
         </div>
