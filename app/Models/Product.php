20718af2f0a5bf6dc31782503f05ce0cabef56f3
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-12 12:00:18 +0800

    add uuid column

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
@@ -11,6 +12,7 @@ class Product extends Model
 
     protected $fillable = [
         'id',
+        'uuid',
         'name',
         'image',
         'qty',
@@ -20,6 +22,8 @@ class Product extends Model
     {
         return $this->belongsToMany(Component::class,'component_product');
     }
+
+    use SoftDeletes;
 
     
     
