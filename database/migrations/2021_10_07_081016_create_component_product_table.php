f5645fb55b115097f9080126a3e670097b4b7184
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-13 17:17:25 +0800

    new component_product table

---
+++ database/migrations/2021_10_07_081016_create_component_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComponentProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('component_product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('component_id')->nullable();
+            $table->unsignedInteger('product_id')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('component_id')->references('id')->on('components')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('component_product');
+    }
+}
