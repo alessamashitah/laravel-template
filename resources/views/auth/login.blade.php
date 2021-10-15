aa220894e5f387097a6aa4802786057fe10d8f6a
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-15 16:16:41 +0800

    add login button for github

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -61,6 +61,16 @@
 
                         </div>
 
+                        <div class="col-md-12 row-block">
+
+                            <a href="{{ url('auth/github') }}" class="btn btn-lg-primary btn-block ">
+
+                            <strong>Login With Github</strong>
+
+                            </a> 
+
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
