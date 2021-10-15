89110890ae3b71824cd5022abde1052f2cc37d83
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-15 16:16:51 +0800

    add route for github

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/redirect', [App\Http\Controllers\GoogleController::class, 'redirectToProvider']);
 Route::get('/callback', [App\Http\Controllers\GoogleController::class, 'handleProviderCallback']);
 
+Route::get('auth/github', [App\Http\Controllers\GithubController::class, 'redirectToProvider']);
+Route::get('github/callback', [App\Http\Controllers\GithubController::class, 'handleProviderCallback']);
+
 Route::get('/', function () {
     return view('welcome');
 });
